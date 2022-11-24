dae87845e7aec523f98d1b35bc717f35fdc09d52
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-11-24 16:25:29 -0300

    0.1.2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const API = express();
 require('./routes')(API);
 API.use(express.json())
 
+API.get('/', (req, res) => {
+    res.send("Express on Vercel");
+})
+
 API.listen(process.env.API_PORT || 8000);
 
 module.exports = API;

0a9698f6a3812ddbae78542720917ef670b6d99e
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-11-25 12:37:23 -0300

    0.1.4

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ require('dotenv').config();
 
 const API = express();
 
-var corsOptions = {
-    origin: process.env.ORIGIN,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+API.use(cors());
 
-API.get('/REQUEST/CORS', cors(corsOptions), async (req, res) => {
-    await fetch(req.headers.endpoint, req.headers.headers).then(async r => {
-        res.status(200).send(await r.json());
-    }).catch(e => res.status(200).send({code: 1001, description: 'Invalid endpoint in request.'}));
+API.get('/', (req,res) => {
+    res.send({states: `ok`})
 })
 
-API.listen(process.env.API_PORT || 80);
+// API.get('/REQUEST/CORS', cors(options), async (req, res) => {
+//     await fetch(req.headers.endpoint, req.headers.headers).then(async r => {
+//         res.status(200).send(await r.json());
+//     }).catch(e => res.status(200).send({code: 1001, description: 'Invalid endpoint in request.'}));
+// })
+
+
+API.listen(process.env.API_PORT || 3001);
 
 module.exports = API;
